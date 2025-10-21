f387df614fc3b0d092cdfa8e9927c318a6e4a15c
Ali <hinnaweali@gmail.com>
2025-10-21 16:28:06 +0200

    adding my first component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 
 import './App.css'
-
+import TestComponent from './components/TestComponent.tsx'
 function App() {
 
   return (
     <>
      <p>My first Typecript project</p>
+     <TestComponent />
     </>
   )
 }
---
+++ src/components/TestComponent.tsx
@@ -0,0 +1,10 @@
+
+export default function TestComponent () {
+
+    return (    
+    <main>
+        <p>Adding first Component</p>
+        <button>test button</button>
+    </main>
+    )
+}
