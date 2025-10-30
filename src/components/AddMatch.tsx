736eeb8d78c9be212bb7c7fd06d1a1b012261104
Ali <hinnaweali@gmail.com>
2025-10-30 16:09:00 +0100

    insert useRef from react to read the values of input texts from the form inputs

--- src/components/AddMatch.tsx
+++ src/components/AddMatch.tsx
@@ -1,17 +1,21 @@
 import type { FormEvent } from "react";
-
+import { useRef } from "react";
 export default function InsetMatch () {
-    function handleSubmit (event : FormEvent) {
+    const matchTitle = useRef<HTMLInputElement>(null);
+    const matchScore = useRef<HTMLInputElement>(null);
+    function handleSubmit (event : FormEvent<HTMLFormElement>) {
         event.preventDefault();
+        const enteredTitle = matchTitle.current!.value;
+        const enteredScore = matchScore.current!.value;
     }
     return (
         <form action="" onSubmit={handleSubmit}>
             <p>Add a Match:</p>
             <label htmlFor="titel">Title: </label>
-            <input type="text" id="title" />
+            <input type="text" id="title" ref={matchTitle}/>
             
             <label htmlFor="score">Score: </label>
-            <input type="text" id="score"/>
+            <input type="text" id="score" ref={matchScore}/>
             <button>Add Match</button>
         </form>
     )
