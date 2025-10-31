39a7159eaa657ce5c0e4950a62c7380bc8efef6d
Ali <hinnaweali@gmail.com>
2025-10-31 14:47:36 +0100

    refine code so that input can be taken from the form and saved in a state

--- src/components/AddMatch.tsx
+++ src/components/AddMatch.tsx
@@ -1,15 +1,27 @@
 import type { FormEvent } from "react";
-import { useRef } from "react";
-export default function InsetMatch () {
+import { useRef, useState } from "react";
+import MatchesList from "./MatchesList";
+import {type singleMatch } from "./Matches";
+export default function AddMatch () {
     const matchTitle = useRef<HTMLInputElement>(null);
     const matchScore = useRef<HTMLInputElement>(null);
-    function handleSubmit (event : FormEvent<HTMLFormElement>) {
+
+    const [matchesList, setMatchesList] = useState <singleMatch []>([]);
+    
+    function handleAddMatch (event : FormEvent<HTMLFormElement>) {
         event.preventDefault();
+        
         const enteredTitle = matchTitle.current!.value;
         const enteredScore = matchScore.current!.value;
+        setMatchesList((previousMatches) => [...previousMatches, {id: Math.random(), title: enteredTitle, score: enteredScore}])
+        
     }
-    return (
-        <form action="" onSubmit={handleSubmit}>
+    function handleDeleteMatch(id: number) {
+        setMatchesList((prev) => prev.filter((match) => match.id !== id));
+    }
+    return ( 
+        <>
+        <form action="" onSubmit={handleAddMatch}>
             <p>Add a Match:</p>
             <label htmlFor="titel">Title: </label>
             <input type="text" id="title" ref={matchTitle}/>
@@ -18,5 +30,7 @@ export default function InsetMatch () {
             <input type="text" id="score" ref={matchScore}/>
             <button>Add Match</button>
         </form>
+        <MatchesList matchesl={matchesList} onDeleteMatch={handleDeleteMatch}/>
+        </>
     )
 }
